d2158e308e4e109d48ad7801a065979f294d7385
Cholmes303 <cj@jasonholmes.net>
2024-09-05 13:11:09 -0600

    Solved counting lines problem

--- line_count.py
+++ line_count.py
@@ -1,4 +1,12 @@
-def line_count():
+def line_count(file):
   """
   Reference the "Reading Files in Python" lesson to open the `file.txt` file, count the number of words in the file, and return the count.
   """
+  file = open('file.txt', 'r') 
+  return file.readlines() + 1
+  
+
+      
+
+
+  
